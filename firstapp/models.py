ee16d1c06d51f3b719e6ad9a5db5a80282712cda
Anitah kawira <annitahkawira@gmail.com>
2017-10-04 14:44:57 +0000

    modified model

--- firstapp/models.py
+++ firstapp/models.py
@@ -3,9 +3,8 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     userId = models.IntegerField(default=1)
-    title = models.CharField("Default Title",max_length = 200)
-    body = models.TextField(default="Body of post goes here")
-    def __str__ (self):
-        return self.title
+    id = models.AutoField("id", primary_key=True)
+    title = models.CharField("Title",max_length = 200)
+    completed = models.TextField(default="false")
     
 
